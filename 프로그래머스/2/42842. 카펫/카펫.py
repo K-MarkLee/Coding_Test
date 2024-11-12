a7ba4a4540cbb52b33e4fb6dcd9fc49dffe62aed
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2024-11-12 10:27:05 +0900

    [level 2] Title: 카펫, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,7 @@
+def solution(brown, yellow):
+    total = brown + yellow  
+    for height in range(3,total -1):  
+        if total % height == 0:  
+            width = total // height
+            if 2 * (width + height) - 4 == brown:
+                return [width, height]
