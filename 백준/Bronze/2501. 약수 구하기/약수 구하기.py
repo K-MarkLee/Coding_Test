f4384ec89b636ef0655d0613ccc543580b928aa9
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2025-08-28 12:58:23 +0900

    [Bronze III] Title: 약수 구하기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,12 @@
+import sys
+N, K = map(int,sys.stdin.readline().strip().split())
+factors = []
+
+for i in range(1,N+1):
+    if N % i == 0:
+        factors.append(i)
+
+if len(factors) < K :
+    print(0)
+else:
+    print(factors[K-1])
