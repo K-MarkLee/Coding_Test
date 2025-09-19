6d0d202cf7631380fcb622a27717d208594ec5c7
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2025-09-19 11:25:38 +0900

    [Bronze II] Title: 블랙잭, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,23 +1,17 @@
 import sys
-n,m = map(int,sys.stdin.readline().strip().split())
-num = list(map(int,sys.stdin.readline().strip().split()))
 
-result = 0
-found = False
+def newblackjack(n,m,cards):
+    final = 0
+    for i in range(n):
+        for j in range(i+1,n):
+            for k in range(j+1,n):
+                total = cards[i]+cards[j]+cards[k]
+                if total == m:
+                    return m
+                elif total < m and total > final:
+                    final = total
+    return final
+                
+n,m,*cards = map(int,sys.stdin.read().strip().split())
 
-for i in range(n):
-    if found:
-        break
-    for j in range(i+1,n):
-        if found:
-            break
-        for k in range(j+1,n):
-            sum = num[i]+num[j]+num[k]
-            if sum == m:
-                print(m)
-                found = True
-                break
-            elif sum > result and sum <= m:
-                result = sum
-if not found:
-    print(result)
+print(newblackjack(n,m,cards))
