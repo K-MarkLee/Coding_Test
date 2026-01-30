b51b472b0f2a762decdfd1fd57bde64986fc6043
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2026-01-30 13:17:08 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.06 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,6 @@
 def solution(array, commands):
     result = []
     for command in commands:
-        new_array = array[command[0]-1:command[1]]
-        new_array.sort()
-        result.append(new_array[command[2]-1])
+        i, j, k = command
+        result.append(list(sorted(array[i-1:j]))[k-1])
     return result
