4322cdc3ceff4f6b3eecef59985dd130c938a1b5
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2026-02-12 20:00:37 +0900

    [level 0] Title: 수 조작하기 2, Time: 27.18 ms, Memory: 12.7 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -2,5 +2,5 @@ def solution(numLog):
     answer = ''
     c_dict = {1:"w", -1:"s", 10:"d", -10:"a"}
     for i in range(1,len(numLog)):
-        answer += c_dict.get(numLog[i]-numLog[i-1],'')
+        answer += c_dict[numLog[i]-numLog[i-1]]
     return answer
