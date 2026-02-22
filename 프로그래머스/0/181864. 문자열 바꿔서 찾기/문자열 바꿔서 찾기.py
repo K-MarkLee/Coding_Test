9350e3ae2fdde2e2c8311c89d0a0cef7f590a687
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2026-02-22 13:10:26 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,8 +1,2 @@
 def solution(myString, pat):
-    pl = list(pat)
-    for i in range(len(pat)):
-        if pat[i] == "A":
-            pl[i] = "B"
-        else:
-            pl[i] = "A"
-    return (1 if "".join(pl) in myString else 0)
+    return int(''.join("A" if i == "B" else "B" for i in pat) in myString)
