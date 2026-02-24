fc1e39535130a83560ebfc686f887071b9b88456
K-MarkLee <70117020+K-MarkLee@users.noreply.github.com>
2026-02-24 21:05:36 +0900

    [level 0] Title: 옹알이 (1), Time: 0.05 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -3,6 +3,4 @@ def solution(babbling):
         babbling[i] = babbling[i].replace("aya","0").replace("ye","0").replace("woo","0").replace("ma","0")
     
     answer = [i for i in babbling if i.isdigit()]
-    if not answer:
-        return 0
     return len(answer)
